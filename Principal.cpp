3259b972268a20b1a7f274f9861377d8b8a3fd8f
JasonDeras <derasjason2@gmail.com>
2019-09-06 14:44:54 -0600

    Creacion del menu y los metodos para agregar un empleado o departamento

--- Principal.cpp
+++ Principal.cpp
@@ -5,9 +5,15 @@
 
 using namespace std;
 
+//Listar
 void MenuListar();
 void Empleados();
 void Departamentos();
+
+//Agregar
+void MenuAgregar();
+void AgregarEmpleado();
+void AgregarDepartamento();
 
 //Variables de control
 sqlite3 *conn;
@@ -30,7 +36,7 @@ int main(int argc, char** argv) {
 		cout<<"3. Eliminar\n\n";
 		cout<<"4. Listado de empleados y jefe\n\n";
 		cout<<"5. Actualizar salario\n\n";
-		cout<<"6. Listado por de partamento\n\n\n";
+		cout<<"6. Listado por de partamento\n\n";
 		cout<<"7. Salir\n\n";
 		cout<<"Ingrese una opcion: ";
 		cin>>op;
@@ -41,6 +47,10 @@ int main(int argc, char** argv) {
 			
 			case 1:
 				MenuListar();
+			break;
+			
+			case 2:
+				MenuAgregar();
 			break;
 			
 			case 7:
@@ -116,17 +126,13 @@ void Empleados(){
 void Departamentos(){
 	
 	error=sqlite3_open("oracle-sample.db",&conn);
-	error=sqlite3_prepare_v2(conn,"select * from emp",1000,&res,&tail);
+	error=sqlite3_prepare_v2(conn,"select * from dept",1000,&res,&tail);
 	
 	while(sqlite3_step(res) == SQLITE_ROW){
 		
-			cout<<"Numero del empleado: "<<sqlite3_column_text(res,0)<<endl;
-			cout<<"Nombre del empleado: "<<sqlite3_column_text(res,1)<<endl;
-			cout<<"Trabajo de empleado: "<<sqlite3_column_text(res,3)<<endl;
-			cout<<"Manager del empleado: "<<sqlite3_column_text(res,4)<<endl;
-			cout<<"Fecha de contratacion: "<<sqlite3_column_text(res,5)<<endl;
-			cout<<"Comimision del empleado: "<<sqlite3_column_text(res,6)<<endl;
-			cout<<"Departamento del empleado: "<<sqlite3_column_text(res,7);
+			cout<<"Numero de Departamento: "<<sqlite3_column_text(res,0)<<endl;
+			cout<<"Nombre del Departamento: "<<sqlite3_column_text(res,1)<<endl;
+			cout<<"Localizacion: "<<sqlite3_column_text(res,2);
 			cout<<"\n\n\n\n";
 	}//Fin del while de impresion
 	
@@ -134,3 +140,99 @@ void Departamentos(){
 	sqlite3_close(conn);
 	
 }//Fin del metodo para listar de departamentos
+
+void MenuAgregar(){
+	
+	int op;
+	
+	cout<<"1. Agregar empleado\n\n";
+	cout<<"2. Agregar departamento\n\n";
+	cout<<"Ingrese una opcion: ";
+	cin>>op;
+	cout<<"\n\n\n\n";
+	
+	switch(op){
+		
+		case 1:
+			AgregarEmpleado();
+		break;
+		
+		case 2:
+			AgregarDepartamento();
+		break;
+		
+		default:
+			cout<<"Opcion no valida\n\n\n\n";
+		break;	
+	}//Fin del switch de listado
+	
+	
+}//Fin del menu para agregar
+
+void AgregarEmpleado(){
+	
+	//Connection con la base
+	error=sqlite3_open("oracle-sample.db",&conn);
+	
+	//Variables para agregar empleados
+	string num_emp;
+	string nom_emp;
+	string trabajo;
+	string manager;
+	string contratacion;
+	string saldo;
+	string commision;
+	string num_dept;
+	
+	//Inserta en la tabla
+	string insert;
+	
+	cout<<"Ingrese numero de empleado: ";
+	cin>>num_emp;
+	
+	cout<<"Ingrese nombre del empleado: ";
+	cin>>nom_emp;
+	
+	cout<<"Ingrese trabajo: ";
+	cin>>trabajo;
+	
+	cout<<"Ingrese manager: ";
+	cin>>manager;
+	
+	cout<<"Fecha de contratacion: ";
+	cin>>contratacion;
+	
+	cout<<"Ingrese saldo: ";
+	cin>>saldo;
+	
+	cout<<"Ingrese comision: ";
+	cin>>commision;
+	
+	cout<<"Ingrese numero de departamento: ";
+	cin>>num_dept;
+	
+	error=sqlite3_exec(conn,insert.c_str(),0,0,0);
+	sqlite3_close(conn);
+	cout<<"\n\n\n\n";
+	
+}//Fin del metodo para listar empleados
+
+void AgregarDepartamento(){
+	
+	error=sqlite3_open("oracle-sample.db",&conn);
+	error=sqlite3_prepare_v2(conn,"select * from dept",1000,&res,&tail);
+	
+	while(sqlite3_step(res) == SQLITE_ROW){
+		
+			cout<<"Numero de Departamento: "<<sqlite3_column_text(res,0)<<endl;
+			cout<<"Nombre del Departamento: "<<sqlite3_column_text(res,1)<<endl;
+			cout<<"Localizacion: "<<sqlite3_column_text(res,2);
+			cout<<"\n\n\n\n";
+	}//Fin del while de impresion
+	
+	cout<<"\n\n\n\n";	
+	sqlite3_close(conn);
+	
+}//Fin del metodo para listar de departamentos
+
+
